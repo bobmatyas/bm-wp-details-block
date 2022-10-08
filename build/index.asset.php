05a65d5a4bc9f0c7e1c196d5145a479cfa38eec7
bob <45246438+bobmatyas@users.noreply.github.com>
2022-10-08 12:54:00 -0400

    add icons and banner

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f00782d827a0aea2b5a6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'cde9b7fe238a01a6e901');
